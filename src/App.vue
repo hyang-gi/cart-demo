9187af63c3411a133a8371ba4764be00e6e72e8c
nikhat <nikhatmohsin22@gmail.com>
2022-03-12 19:23:01 +0530

    updated code

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,5 @@
+/* eslint-disable */
+// eslint-disable-next-line
 <template>
   <header class="top-bar spread">
     <nav class="top-bar-nav">
@@ -11,38 +13,61 @@
       <router-link to="past-orders" class="top-bar-link pa-2">
         <span>Past Orders</span>
       </router-link>
-      <button @click="toggleSidebar" class="top-bar-cart-link pa-2">
-        <i class="icofont-cart-alt icofont-1x"></i>
-        <span>Cart ({{ totalQuantity }})</span>
-      </button>
     </nav>
+    <div @click="toggleSidebar" class="top-bar-cart-link">
+      <i class="icofont-cart-alt icofont-1x"></i>
+      <span>Cart ({{ totalQuantity }})</span>
+    </div>
   </header>
-  <router-view />
+  <router-view :inventory="inventory" />
+
+  <Sidebar
+    v-if="showSideBar"
+    :toggle="toggleSidebar"
+    :cart="cart"
+    :inventory="inventory"
+    :remove="removeItem"
+  />
 </template>
 
 <script>
+import Sidebar from "./components/Sidebar.vue";
+import food from "./food.json";
+
 export default {
   name: "App",
+  components: {
+    Sidebar,
+  },
   data() {
     return {
-      totalQuantity: 10,
+      showSideBar: false,
+      inventory: food,
+      cart: {},
     };
   },
+  computed: {
+    totalQuantity() {
+      return Object.values(this.cart).reduce((acc, curr) => {
+        return acc + curr;
+      }, 0);
+    },
+  },
   methods: {
+    addToCart(name, index) {
+      if (!this.cart[name]) this.cart[name] = 0;
+      console.log(name, index);
+      this.cart[name] += this.inventory[index].quantity;
+      this.inventory[index].quantity = 0;
+      console.log("cart", this.cart);
+    },
     toggleSidebar() {
-      console.log("toggling sidebar");
+      this.showSidebar = !this.showSidebar;
+      console.log("Click", this.showSideBar);
+    },
+    removeItem(name) {
+      delete this.cart[name];
     },
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Products from '../views/Products.vue'
+import PastOrders from '../views/PastOrders.vue'
 
 const routes = [
     {
@@ -12,6 +13,11 @@ const routes = [
         path: '/products',
         name: 'Products',
         component: Products
+    },
+    {
+        path: '/past-orders',
+        name: 'Past Orders',
+        component: PastOrders,
     }
 ]
 
@@ -19,6 +25,6 @@ const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
     routes
 })
-  
+
 
 export default router;
